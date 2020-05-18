f7449f180ef7b601a73d81d71bae7a7e74c5b2c2
Iran R <iranthar@rent.com.au>
2020-05-19 03:10:02 +0800

    Add product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,20 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  subject(:product) do
-    FactoryBot.create(
-      :product,
-      name: 'eth',
-      price: 331.100000000,
-      last: 314.10000000,
-      bid: 313.80000000,
-      ask: 315.40000000
-    )
-  end
+  subject(:product) { FactoryBot.create(:product, :eth) }
 
-  it { expect(product.name).to eq 'eth' }
-  it { expect(product.price).to eq 331.1 }
-  it { expect(product.last).to eq 314.1 }
-  it { expect(product.bid).to eq 313.8 }
-  it { expect(product.ask).to eq 315.4 }
+  it { is_expected.to have_many(:prices) }
+  it { expect(product.name).to eq 'ETHAUD' }
 end
